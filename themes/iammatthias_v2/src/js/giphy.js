1ca1c4d769b059da6d0a39471e4f0671ed39fbc2
Matthias Jordan <matthias.k.jordan@gmail.com>
2017-03-05 17:04:08 -0800

    config / code+pre / 404

--- themes/iammatthias_v2/src/js/giphy.js
+++ themes/iammatthias_v2/src/js/giphy.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
 	q = "finger guns"; // search query
 
 	request = new XMLHttpRequest;
-	request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=g&tag=404+not+found'+q, true);
+	request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg&tag=arrested-development+404-not-found'+q, true);
 
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400){
 			data = JSON.parse(request.responseText).data.image_url;
 			console.log(data);
-			document.getElementById("giphyme").innerHTML = '<center><img src = "'+data+'"  title="404 Gif - Powered by GIPHY"></center>';
+			document.getElementById("giphyme").innerHTML = '<center><img width="100%" src = "'+data+'"  title="404 Gif - Powered by GIPHY"></center>';
 		} else {
 			console.log('reached giphy, but API returned an error');
 		 }
